68ae24d42e9963dc4c26cd991499edaa9884f6bd
Wendy Harris <wendyv510@hotmail.com>
2019-09-23 21:58:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,12 @@ def summon_captain_planet
     find_all(4 letters)
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
+  short_words=["puff", "go", "two"]
+  long_planeteer_calls(short_words)
+  
+  assorted_words = ["two", "go", "industious", "bop"]
+  long_planeteer_calls(assorted_words)
 end
 
 def find_the_cheese# code an argument here
